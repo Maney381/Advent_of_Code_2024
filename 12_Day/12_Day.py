f2e18493006a0310ba6ad3fd6d2933564e99dc58
christian <c.g.neckel@gmail.com>
2024-12-13 15:57:38 +0100

    Finished Day 12

--- 12_Day/12_Day.py
+++ 12_Day/12_Day.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-with open("Advent_of_Code_2024/12_Day/sample_data.txt", "r") as lists_data:
+with open("Advent_of_Code_2024/12_Day/data.txt", "r") as lists_data:
     data = []
     while True:
         line = lists_data.readline().strip('\n')
@@ -54,8 +54,26 @@ for i in range(data_np.shape[0]):
             dic[number_of_fields] = [[i, j]]
             check_neighbours(cur_letter, i, j)
 
-for row in data_np:
-    print(row)
-
+total_sum = 0
 for key, value in dic.items():
-    print(key, value)
+    sum_field = 0
+    for v1 in value:
+        up, down, right, left = 1, 1, 1, 1
+        for v2 in value:          
+            if v1 != v2:
+                #does right exist
+                if v1[0] == v2[0] and v1[1] == v2[1]+1:
+                    right = 0
+                #does left exist
+                if v1[0] == v2[0] and v1[1] == v2[1]-1:
+                    left = 0
+                #does up exist
+                if v1[0] == v2[0]+1 and v1[1] == v2[1]:
+                    up = 0
+                #does down exist
+                if v1[0] == v2[0]-1 and v1[1] == v2[1]:
+                    down = 0
+        sum_field += sum([up, down, right, left])
+    total_sum += sum_field * len(value)
+
+print(total_sum)
